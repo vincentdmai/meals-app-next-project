784ff63cbf840921af8078e32b1cca6c47f975ae
Vincent <46911814+vincentdmai@users.noreply.github.com>
2024-01-23 23:32:59 +0000

    Refactored Navigation Link for Styling

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "@/components/main-header/nav-link";
 
 const MainHeader = () => {
   return (
@@ -18,10 +19,10 @@ const MainHeader = () => {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Community</Link>
+              <NavLink href="/community">Community</NavLink>
             </li>
           </ul>
         </nav>
